d17bd51cec733501d84a8a15a55d77069ba68f4e
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-04-10 19:03:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Greeting from './components/Greeting';
+import Message from './components/Message';
+import Button from './components/Button';
 
 function App() {
+  const handleClick = () => {
+    console.log("Кнопку натиснули!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greeting name="Semma" />
+      <Message text="Це довільне повідомлення для користувача." />
+      <Button onClick={handleClick} />
     </div>
   );
 }
---
+++ src/components/Greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greeting = ({ name }) => {
+  return <h1>Привіт, {name}!</h1>;
+};
+
+export default Greeting;
